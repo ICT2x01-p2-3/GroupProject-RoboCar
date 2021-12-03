69a0f2636ecbc7196f29492fa43dc259cfd9c152
DanialAshidiq <77533235+DanialAshidiq@users.noreply.github.com>
2021-12-03 16:56:38 +0800

    whitebox testing update
    challenge.py : 100% statement coverage
    PinControl.oy: 96% statement coverage

--- whiteboxtesting/challenge/PinControl.py
+++ whiteboxtesting/challenge/PinControl.py
@@ -1,9 +1,13 @@
-from types import DynamicClassAttribute
-from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///mazedriver.db'
-db = SQLAlchemy(app)
+from application import db
 import string
 import random
+
+
+class pin_table(db.Model):
+    Id = db.Column(db.Integer(), primary_key=True)
+    pin = db.Column(db.String(length=50), nullable=False, unique=True)
+
+
 class Pin_Control:
 
     def GetPin(self):
@@ -24,3 +28,32 @@ class Pin_Control:
         if pin == query[0].pin:
             return True
         return False
+    
+
+    def testCase1():
+        Id = pin_table.Id
+        pin = pin_table.pin
+        x = Pin_Control.GetPin(self=pin_table.Id)
+        print("\nTest Case 1: Get all pins from Pin Class and pin_table database\n")
+        assert x, "ID Object 1"
+        print(x)
+
+    def testCase2():
+        try:
+            print("\nTest Case 2: Generate New Pin to insert into pin_table database\n")
+            assert Pin_Control.GeneratePin(self=1), "New Pin inserted"
+            print(Pin_Control.GetPin(self=1))
+        except AssertionError as error:
+            print(error)
+    def testCase3():
+        try:
+            fakepin = 11212
+            assert Pin_Control.comparePin(fakepin,pin=pin_table.pin)
+        except AssertionError as error:
+            print(error)
+
+
+
+Pin_Control.testCase1()
+Pin_Control.testCase2()
+Pin_Control.testCase3()
